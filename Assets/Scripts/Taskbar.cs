b96ce73ff7eaacf839b62154005705e22520b90e
soraaaaaaaaaaaaa <souponion70@gmail.com>
2026-05-19 16:21:57 +0300

    taskbar

--- Assets/Scripts/Taskbar.cs
+++ Assets/Scripts/Taskbar.cs
@@ -1,13 +1,17 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Taskbar : MonoBehaviour
 {
+    [SerializeField] Slider slider;
+    [SerializeField, Range(0, 2)] int index;
     private void Start()
     {
-        ZoneManager.taskbars.Add(this);
+        ZoneManager.taskbars[index] = this;
     }
     public void UpdateTaskbar(float percentage)
     {
+        slider.value = percentage;
         Debug.Log(percentage * 100f);
     }
 }
--- Assets/Scripts/ZoneManager.cs
+++ Assets/Scripts/ZoneManager.cs
@@ -6,21 +6,20 @@ public class ZoneManager : MonoBehaviour
     // storing tasks in an array for easy managing
     public static int[] zoneTasks = new int[3];
     public static int[] zoneTasksCompleted = new int[3];
-    public static List<Taskbar> taskbars;
+    public static Taskbar[] taskbars = new Taskbar[3];
     public static void AddTask(int i)
     {
         zoneTasks[i]++;
         taskbars[i].UpdateTaskbar((float)zoneTasksCompleted[i] / zoneTasks[i]);
-        Debug.Log((float)zoneTasksCompleted[i]/zoneTasks[i] * 100);
     }
     public static void TaskCompleted(int i)
     {
         zoneTasksCompleted[i]++;
-        //Debug.Log((float)zoneTasksCompleted[i] / zoneTasks[i] * 100);
+        taskbars[i].UpdateTaskbar((float)zoneTasksCompleted[i] / zoneTasks[i]);
     }
     public static void TaskUndone(int i)
     {
         zoneTasksCompleted[i]--;
-        Debug.Log((float)zoneTasksCompleted[i] / zoneTasks[i] * 100);
+        taskbars[i].UpdateTaskbar((float)zoneTasksCompleted[i] / zoneTasks[i]);
     }
 }
