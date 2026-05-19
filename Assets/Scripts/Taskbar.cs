f57ef7c8bf8d46f24768e96ecefd8ef39b4ffed1
soraaaaaaaaaaaaa <souponion70@gmail.com>
2026-05-19 16:28:11 +0300

    taskbar

--- Assets/Scripts/Taskbar.cs
+++ Assets/Scripts/Taskbar.cs
@@ -5,6 +5,7 @@ public class Taskbar : MonoBehaviour
 {
     [SerializeField] Slider slider;
     [SerializeField, Range(0, 2)] int index;
+    float halfCompleted = 0.33f;
     private void Start()
     {
         ZoneManager.taskbars[index] = this;
@@ -12,6 +13,10 @@ public class Taskbar : MonoBehaviour
     public void UpdateTaskbar(float percentage)
     {
         slider.value = percentage;
+        if(percentage >= 1f)
+        {
+
+        }
         Debug.Log(percentage * 100f);
     }
 }
